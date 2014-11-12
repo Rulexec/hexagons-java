2054ec4b13c2ff167644f02510159590e1cabbdc
Alexander Ruliov <alexander.ruliov@gmail.com>
2014-11-12 14:27:40 +0300

    onCanReadReturnable now is default

--- src/api/java/by/muna/io/returnable/IAsyncReturnableInputStream.java
+++ src/api/java/by/muna/io/returnable/IAsyncReturnableInputStream.java
@@ -3,8 +3,10 @@ package by.muna.io.returnable;
 import by.muna.io.ByteReaderUtil;
 import by.muna.io.IAsyncByteInputStream;
 import by.muna.io.IByteReader;
+import by.muna.io.IByteWriter;
 import by.muna.monads.IAsyncFuture;
 
+import java.nio.ByteBuffer;
 import java.util.function.Function;
 
 public interface IAsyncReturnableInputStream extends IAsyncByteInputStream, IReturnableInput {
@@ -13,7 +15,54 @@ public interface IAsyncReturnableInputStream extends IAsyncByteInputStream, IRet
         this.onCanReadReturnable(reader::apply);
     }
 
-    void onCanReadReturnable(Function<IReturnableByteReader, Boolean> reader);
+    default void onCanReadReturnable(Function<IReturnableByteReader, Boolean> listener) {
+        this.onCanRead(reader -> listener.apply(new IReturnableByteReader() {
+            @Override
+            public int available() {
+                return reader.available();
+            }
+
+            @Override
+            public int read(byte[] buffer) {
+                return reader.read(buffer);
+            }
+
+            @Override
+            public int read(byte[] buffer, int offset) {
+                return reader.read(buffer, offset);
+            }
+
+            @Override
+            public int read(byte[] buffer, int offset, int length) {
+                return reader.read(buffer, offset, length);
+            }
+
+            @Override
+            public int read(ByteBuffer buffer) {
+                return reader.read(buffer);
+            }
+
+            @Override
+            public int read(IByteWriter writer) {
+                return reader.read(writer);
+            }
+
+            @Override
+            public void end() {
+                reader.end();
+            }
+
+            @Override
+            public boolean isEnded() {
+                return reader.isEnded();
+            }
+
+            @Override
+            public void returnInput(IByteReader rest) {
+                IAsyncReturnableInputStream.this.returnInput(rest);
+            }
+        }));
+    }
 
     @Override
     default IAsyncFuture<Object> end() {
